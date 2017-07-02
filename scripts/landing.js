cf0b15d0bf2a602568778b81bc0e14cc5e4e20be
Don DiProva <ddiprova@aol.com>
2017-07-02 15:12:41 -0700

    'checkpoint-10-collections'

--- scripts/landing.js
+++ scripts/landing.js
@@ -18,6 +18,7 @@ window.onload = function() {
   var sellingPoints = document.getElementsByClassName('selling-points')[0];
   var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
   window.addEventListener('scroll', function(event) {
+    console.log(event);
     if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
               animatePoints(pointsArray);
           }
