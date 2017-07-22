214b6b0234b67d8013b8173cf30d6ec9df24c1ac
Don DiProva <ddiprova@aol.com>
2017-07-22 12:27:37 -0700

    checkpoint-13-scripting-part-b completed

--- scripts/album.js
+++ scripts/album.js
@@ -12,6 +12,7 @@ var albumPicasso = {
         { title: 'Magenta', duration: '2:15'}
     ]
 };
+
 var albumMarconi = {
     title: 'The Telephone',
     artist: 'Guglielmo Marconi',
@@ -26,6 +27,7 @@ var albumMarconi = {
         { title: 'Wrong phone number', duration: '2:15'}
     ]
 };
+
 var albumEinstein = {
     title: 'Theory of Relativity',
     artist: 'Albert Einstein',
@@ -52,6 +54,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     return template;
 };
+
 var albumTitle = document.getElementsByClassName('album-view-title')[0];
 var albumArtist = document.getElementsByClassName('album-view-artist')[0];
 var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
@@ -59,39 +62,90 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 var setCurrentAlbum = function(album) {
 
-
-
-     albumTitle.firstChild.nodeValue = album.title;
-     albumArtist.firstChild.nodeValue = album.artist;
-     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
-     albumImage.setAttribute('src', album.albumArtUrl);
-
-     albumSongList.innerHTML = '';
+albumTitle.firstChild.nodeValue = album.title
+albumArtist.firstChild.nodeValue = album.artist;
+albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+albumImage.setAttribute('src', album.albumArtUrl);
+albumSongList.innerHTML = '';
 
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
  };
- var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
- var songRows = document.getElementsByClassName('album-view-song-item');
- var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+ var findParentByClassName = function(element, targetClass) {
+     if (element) {
+         var currentParent = element.parentElement;
+         while (currentParent.className !== targetClass && currentParent.className !== null) {
+               currentParent = currentParent.parentElement;
+           }
+           return currentParent;
+       }
+   };
+   var getSongItem = function(element) {
+       switch (element.className) {
+           case 'album-song-button':
+           case 'ion-play':
+           case 'ion-pause':
+               return findParentByClassName(element, 'song-item-number');
+           case 'album-view-song-item':
+               return element.querySelector('.song-item-number');
+           case 'song-item-title':
+           case 'song-item-duration':
+               return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
+           case 'song-item-number':
+               return element;
+           default:
+               return;
+       }
+     };
+     var clickHandler = function(targetElement) {
+         var songItem = getSongItem(targetElement);
+         if (currentlyPlayingSong === null) {
+            songItem.innerHTML = pauseButtonTemplate;
+            currentlyPlayingSong = songItem.getAttribute('data-song-number');
+       } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
+              songItem.innerHTML = playButtonTemplate;
+              currentlyPlayingSong = null;
+            } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
+             var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
+             currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
+             songItem.innerHTML = pauseButtonTemplate;
+             currentlyPlayingSong = songItem.getAttribute('data-song-number');
+         }
+       };
+       
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+var currentlyPlayingSong = null;
 
- window.onload = function() {
+window.onload = function() {
      setCurrentAlbum(albumPicasso);
      songListContainer.addEventListener('mouseover', function(event) {
-             // #1
              console.log(event.target);
      if (event.target.parentElement.className === 'album-view-song-item') {
-        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-         }
+        var songItem = getSongItem(event.target);
+            if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
+                songItem.innerHTML = playButtonTemplate;
+            }
+     }
      });
-      // Change the content from the number to the play button's HTML
+
       for (var i = 0; i < songRows.length; i++) {
           songRows[i].addEventListener('mouseleave', function(event) {
-              // Revert the content back to the number
-              this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+            var songItem = getSongItem(event.target);
+            var songItemNumber = songItem.getAttribute('data-song-number');
+
+            if (songItemNumber !== currentlyPlayingSong) {
+                songItem.innerHTML = songItemNumber;
+            }
           });
+          songRows[i].addEventListener('click', function(event) {
+             clickHandler(event.target);
+        });
       }
+
      var albums = [albumPicasso, albumMarconi, albumEinstein];
      var index = 1;
      albumImage.addEventListener("click", function(event) {
@@ -101,4 +155,4 @@ var setCurrentAlbum = function(album) {
             index = 0;
         }
       });
- };
+      };
